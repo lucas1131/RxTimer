f60af77e59a1566f0a508e52da9dea0c675b1113
Lucas Soares <lucassoares1793@gmail.com>
2025-03-20 15:45:17 -0400

    Ignore zenject test that depends on adding zenject scene to build settings

--- Assets/Plugins/Zenject/OptionalExtras/IntegrationTests/Tests/SceneTests/TestSceneContextEvents.cs
+++ Assets/Plugins/Zenject/OptionalExtras/IntegrationTests/Tests/SceneTests/TestSceneContextEvents.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
@@ -6,6 +7,7 @@ namespace Zenject.Tests
 {
     public class TestSceneContextEvents : SceneTestFixture
     {
+        [Ignore("This is internal Zenject SceneTestFixture and it needs to add their test scene to the build settings, but I don't want to add a Zenject scene dependency to the project.")]
         [UnityTest]
         public IEnumerator TestScene()
         {
